a6676dc3ca08e19ef744dc6fef0a956b114b2887
Diego Orozco <dorozco@myseneca.ca>
2025-05-28 15:37:02 -0400

    Lab 2 Complete

---
+++ lab2e.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Author: Diego Orozco
+# Author ID: dorozco
+# Date Created: 2025/05/28
+
+timer = 10
+
+while timer > 0:
+    print(timer)
+    timer = timer - 1
+
+print('blast off!')
---
+++ lab2f.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Author: Diego Orozco
+# Author ID: dorozco
+# Date Created: 2025/05/28
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer > 0:
+    print(timer)
+    timer = timer - 1
+
+print('blast off!')
